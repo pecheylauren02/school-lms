8b046be61bca8bce49d28d6d4e6a984bf1fbb57d
Lauren Pechey <118538771+pecheylauren02@users.noreply.github.com>
2024-09-29 15:46:25 +0400

    add static folders and make migrations

--- school_management/core/views.py
+++ school_management/core/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
 
-from django.shortcuts import render
-
 def home(request):
     return render(request, 'core/base.html')
--- school_management/school_management/settings.py
+++ school_management/school_management/settings.py
@@ -119,8 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [BASE_DIR / "static"]
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'core/static')]
+STATICFILES_DIRS = [BASE_DIR / "static"]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'core/static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
