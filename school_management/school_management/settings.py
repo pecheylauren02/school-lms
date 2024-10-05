07cfd58b8ed74230adc3f7cf42c1faa0ef64e336
Lauren Pechey <118538771+pecheylauren02@users.noreply.github.com>
2024-10-05 10:02:02 +0400

    setup allauth

--- school_management/school_management/settings.py
+++ school_management/school_management/settings.py
@@ -87,6 +87,7 @@ SITE_ID = 1
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+# Allows email verification
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
